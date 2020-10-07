ddf51ae7d92d5d284169db1e4e0edacac6d3a956
Delor Rotstrump <delor@delor.pl>
2020-10-07 18:51:43 +0200

    initial commit

---
+++ src/components/CarceresMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Hello from Carceres!!!
+  </div>
+</template>
+
+<script>
+//const axios = require('axios').default;
+
+export default {
+   data: function() {
+        return {
+            info: null,
+            loading: true,
+            errored: false,
+        }
+    },
+  methods: {
+  },
+  mounted () {
+  },
+  components: {
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Carceres from '@/components/CarceresMain.vue'
+//import router from './router'
+import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  //router,
+  store,
+  render: h => h(Carceres)
+}).$mount('#carceres')
