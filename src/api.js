0f64b0bc3edcd2cde73e6e275bfdf7c05d63afd4
Delor Rotstrump <delor@delor.pl>
2020-10-29 19:30:57 +0100

    cleaning code in api.js

--- src/api.js
+++ src/api.js
@@ -15,14 +15,8 @@ function removeLocalStorageTokens() {
   localStorage.removeItem("access_token");
   localStorage.removeItem("refresh_token");
 }
-
-function errorHandle(error) {
-  console.log('Http error: ', error, error.response);
-
-  if (error.response.status == 401) {
-    return api.refresh() //TODO: need rerun request
-  }
-  return Promise.reject(error);
+function checkLocalStorageTokens() {
+  return !(!getAccessToken() || !getRefreshToken())
 }
 
 class Api {
@@ -38,47 +32,11 @@ class Api {
       config.headers.common['x-access-tokens'] = getAccessToken();
       return config;
     });
-    if (getAccessToken()) {
-      this.auth.authorized = true;
-    }
-  }
-
-  auth = {
-    authorized: false,
-  }
-  refreshToken = async function () {
-    return await this.api.post(`/api/refresh`, { refresh_token: getRefreshToken() });
-  }
-
-  /* METHODS */
-  get = async function (...args) {
-    return await this.api.get.apply(this.api, args).then(this.getData).catch(errorHandle);
-  }
-  post = async function (...args) {
-    return await this.api.post.apply(this.api, args).then(this.getData).catch(errorHandle);
-  }
-  put = async function (...args) {
-    return await this.api.put.apply(this.api, args).then(this.getData).catch(errorHandle);
-  }
-  delete = async function (...args) {
-    return await this.api.delete.apply(this.api, args).then(this.getData).catch(errorHandle);
-  }
-
-  /* response handling */
-  getData = function (response) {
-    //console.log(response.config.method, response.config.url, "->", response.status)
-    return response.data;
-  }
-
-  refresh = async function () {
-    var resp = await this.refreshToken()
-    setLocalStorageTokens(resp.data)
-    this.auth.authorized = true;
+    this._updateAuthorizedStatus()
   }
 
   /* login */
   login = async function (login, password) {
-
     var tokens = await this.api.get(`/api/login`, {
       auth: {
         username: login,
@@ -86,23 +44,82 @@ class Api {
       }
     }
     )
-    setLocalStorageTokens(tokens.data)
-    this.auth.authorized = true;
+    api._saveTokens(tokens.data)
   }
 
-  logout = async function () {
-    removeLocalStorageTokens()
-    this.auth.authorized = false;
+  logout = function () {
+    api._removeTokens()
+  }
+
+  /* METHODS */
+  get = async function (...args) {
+    return await this.api.get.apply(this.api, args).then(this._getData).catch(this._errorHandle);
+  }
+  post = async function (...args) {
+    return await this.api.post.apply(this.api, args).then(this._getData).catch(this._errorHandle);
+  }
+  put = async function (...args) {
+    return await this.api.put.apply(this.api, args).then(this._getData).catch(this._errorHandle);
+  }
+  delete = async function (...args) {
+    return await this.api.delete.apply(this.api, args).then(this._getData).catch(this._errorHandle);
   }
 
   /* API ENDPOINTS */
   getUser = async function () {
-
     return await this.get("/api/user");
   }
   getUsers = async function () {
     return await this.get("/api/users");
   }
+
+  /* response handling */
+  /* Response to methods this.get(), this.post(), ... */
+  _getData = function (response) {
+    //console.log(response.config.method, response.config.url, "->", response.status)
+    return response.data;
+  }
+
+  /* Errors in methods this.get(), this.post(), ... */
+  _errorHandle = function (error) {
+    if (error.response.status == 401) {
+      console.log('Refreshing token...');
+      return api._refresh(error)
+    }
+    console.log('Http error: ', error);
+    return Promise.reject(error);
+  }
+
+  _refreshToken = async function () {
+    return await this.api.post(`/api/refresh`, { refresh_token: getRefreshToken() });
+  }
+
+  _refresh = async function (err) {
+    return await this._refreshToken().then(resp => {
+      /* save tokens */
+      api._saveTokens(resp.data)
+      /* rerun request with new token */
+      err.config.headers['x-access-tokens'] = getAccessToken();
+      return api.api.request(err.config)
+    });
+  }
+
+  auth = {
+    authorized: false,
+  }
+  _updateAuthorizedStatus() {
+    this.auth.authorized = checkLocalStorageTokens()
+  }
+  _removeTokens() {
+    removeLocalStorageTokens()
+    api._updateAuthorizedStatus()
+  }
+
+  _saveTokens(tokens) {
+    setLocalStorageTokens(tokens)
+    api._updateAuthorizedStatus()
+  }
+
 }
 
 const api = new Api()
