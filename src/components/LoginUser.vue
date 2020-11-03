992f1c37062431ca744bf349dee8fb93f89f9852
SebekBr <sebastian.brodziak@gmail.com>
2020-11-04 00:33:40 +0100

    Login inputs no radius

--- src/components/LoginUser.vue
+++ src/components/LoginUser.vue
@@ -3,8 +3,8 @@
   <!--Logowanie-->
   <div style="width: 40%; background-color:#404040; padding: 30px 0px 30px 0px; margin: 0 auto; border-radius: 25px;">
     <b-form v-on:submit.prevent="login()" :hidden="auth.authorized">
-      <b-form-input type="text" v-model="name" placeholder="Login" />
-      <b-form-input type="password" v-model="pass" placeholder="Hasło" />
+      <b-form-input type="text" v-model="name" placeholder="Login" style="border-radius: 0;"/>
+      <b-form-input type="password" v-model="pass" placeholder="Hasło" style="border-radius: 0;"/>
       <b-button pill variant="success" @click="login()" style="margin-top: 30px;"> Zaloguj </b-button>
     </b-form>
     <span :hidden="!auth.authorized">Witaj {{ user_name }}.</span>
