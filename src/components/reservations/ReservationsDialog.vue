0d9c654d238250416cae4ecdbd909e8cd2c2da2e
Delor Rotstrump <delor@delor.pl>
2020-11-04 16:26:58 +0100

    added reservations card files

---
+++ src/components/reservations/ReservationsDialog.vue
@@ -0,0 +1,138 @@
+<template>
+  <b-modal
+    :id="modal_id"
+    :title="modalTitle"
+    @backdrop="ok()"
+    @ok="ok()"
+    @hide="$emit('hide-modal', $event)"
+    @show="onShow()"
+  >
+    <b-form v-on:submit.prevent="ok()">
+      <b-form-group
+        id="input-group-car"
+        label="Samochód:"
+        label-for="input-car"
+      >
+        <b-form-select id="input-car" v-model="model.car_id" required>
+          <b-form-select-option-group
+            v-for="client in clients"
+            v-bind:key="client.id"
+            :label="client.name"
+          >
+            <b-form-select-option
+              v-for="car in client.cars"
+              v-bind:key="car.id"
+              :value="car.id"
+            >
+              {{ car.plate }}
+            </b-form-select-option>
+          </b-form-select-option-group>
+        </b-form-select>
+      </b-form-group>
+      <b-form-group
+        id="input-group-place"
+        label="Miejsce:"
+        label-for="input-place"
+      >
+        <b-form-select id="input-place" v-model="model.place_id" required>
+          <b-form-select-option :value="null">
+            Wybierz miejsce:
+          </b-form-select-option>
+          <b-form-select-option-group
+            v-for="zone in zones"
+            v-bind:key="zone.id"
+            :label="zone.name"
+          >
+            <b-form-select-option
+              v-for="place in zone.places"
+              v-bind:key="place.id"
+              :value="place.id"
+              :disabled="place.occupied"
+            >
+              Miejsce nr {{ place.nr }} {{ place.name }}
+              {{ place.occupied ? "(niedostępne)" : "" }}
+            </b-form-select-option>
+          </b-form-select-option-group>
+        </b-form-select>
+      </b-form-group>
+      <b-form-group
+        id="input-group-datetime"
+        label="Rezerwacja do:"
+        label-for="input-enddate"
+      >
+        <datetime
+          id="input-enddate"
+          format="YYYY-MM-DD H:i"
+          width="300px"
+          v-model="model.end"
+        ></datetime>
+      </b-form-group>
+    </b-form>
+
+    <template #modal-footer="{ ok, cancel, hide }">
+      <b-button
+        :hidden="model.id == -1"
+        variant="danger"
+        @click="
+          $emit('remove-model', model.id);
+          hide();
+        "
+      >
+        Usuń
+      </b-button>
+      <b-button variant="secondary" @click="cancel()"> Anuluj </b-button>
+      <b-button variant="success" @click="ok()">
+        {{ model.id != -1 ? "Zapisz" : "Stwórz" }}
+      </b-button>
+    </template>
+  </b-modal>
+</template>
+
+<script>
+import datetime from "vuejs-datetimepicker";
+
+export default {
+  components: { datetime },
+  data: function () {
+    return {
+      zones: {},
+      clients: {},
+      loading: false,
+    };
+  },
+  computed: {
+    modalTitle() {
+      return `${this.model.id == -1 ? "Tworzenie" : "Edycja"} rezerwacji`;
+    },
+  },
+  methods: {
+    ok() {
+      this.$emit("submit-edit", this.model);
+      this.$bvModal.hide(this.modal_id);
+    },
+    async onShow() {
+      await this.loadPlaces();
+    },
+    async loadClients() {
+      this.clients = await this.api.getAll(this.api.getClients);
+    },
+    async loadPlaces() {
+      this.zones = await this.api.getAll(this.api.getZones);
+    },
+    async loadResources() {
+      this.loading = true;
+
+      await this.loadClients();
+
+      this.loading = false;
+    },
+  },
+  mounted() {
+    this.loadResources();
+  },
+  props: ["model", "modal_id"],
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/reservations/ReservationsList.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <b-card-group deck>
+      <b-card class="reservation_list">
+        <b-card-title>Rezerwacje</b-card-title>
+        <b-card-sub-title>
+          <span
+            role="button"
+            @click="onCreateModel"
+            :class="{ 'd-none': !!dialogFormVisible }"
+          >
+            <b-icon-plus-circle></b-icon-plus-circle>
+            Nowy
+          </span>
+        </b-card-sub-title>
+        <reservations-dialog
+          :model="formModel"
+          :modal_id="modal_dialog_id"
+          v-on:cancel-edit="onCancelEdit()"
+          v-on:submit-edit="onSubmitEdit($event)"
+          v-on:hide-modal="resetDialog()"
+          v-on:remove-model="onRemoveModel($event)"
+        ></reservations-dialog>
+        <b-list-group-item v-for="model in models" v-bind:key="model.id">
+          <span role="button" @click.prevent="onEditModel(model.id)">
+            <b-icon-caret-right></b-icon-caret-right>
+            Miejsce: {{ model.place_id }} Samochód: {{ model.car_id }} Start:
+            {{ model.start }} End: {{ model.end }} Typ: {{ model.type }}
+          </span>
+        </b-list-group-item>
+      </b-card>
+    </b-card-group>
+  </div>
+</template>
+
+<script>
+import ReservationsDialog from "@/components/reservations/ReservationsDialog.vue";
+
+export default {
+  components: {
+    "reservations-dialog": ReservationsDialog,
+  },
+  data: function () {
+    return {
+      models: {},
+      dialogFormVisible: false,
+      loading: false,
+      formModel: {},
+      modal_dialog_id: "reservations-dialog-modal",
+      clients: {},
+    };
+  },
+  methods: {
+    _newModel() {
+      return {
+        id: -1,
+        start: "",
+        end: "",
+        type: 1,
+        car_id: -1,
+        place_id: -1,
+      };
+    },
+    _cloneModel(model) {
+      var _model = this._newModel();
+      _model.id = model.id;
+      _model.start = model.start;
+      _model.end = model.end;
+      _model.type = model.type;
+      _model.car_id = model.car_id;
+      _model.place_id = model.place_id;
+      return _model;
+    },
+    onCreateModel() {
+      this.formModel = this._newModel();
+      this.dialogFormVisible = true;
+      this.$bvModal.show(this.modal_dialog_id);
+    },
+    onEditModel() {
+      /* TODO: implementing this */
+      // TODO: show msg to user
+    },
+    onRemoveModel(model_id) {
+      this.loading = true;
+      this.deleteModel(model_id);
+    },
+    onCancelEdit() {
+      this.resetDialog();
+    },
+    onSubmitEdit(model) {
+      this.loading = true;
+      if (model.id != -1) this.updateModel(model);
+      else this.saveModel(model);
+    },
+    async saveModel(model) {
+      /* Send new client's data to api */
+      model.start = null;
+      model.end = new Date(Date.parse(model.end)).toISOString();
+      var created = await this.api.createSubscription(model);
+      /* Add to list */
+      this.models.push(created);
+      this.loading = false;
+      this.resetDialog();
+    },
+    async updateModel() {
+      /* TODO: implementing this */
+      this.loading = false;
+      this.resetDialog();
+    },
+    async deleteModel() {
+      /* removing not possible */
+      this.loading = false;
+    },
+    resetDialog() {
+      this.formModel = this._newModel();
+      this.dialogFormVisible = false;
+    },
+    async loadModels() {
+      this.clients = await this.api.getAll(this.api.getClients);
+      var page = await this.api.getSubscriptions('desc(end)');
+      var pages = page.results;
+      while (page.hasNext()) {
+        page = await this.api.getNext(page);
+        pages.push(...page.results);
+      }
+      this.models = pages;
+
+      this.loading = false;
+    },
+  },
+  mounted() {
+    this.loading = true;
+    this.loadModels();
+    this.resetDialog();
+  },
+
+  props: ["shared_data"],
+};
+</script>
+<style scoped>
+</style>
